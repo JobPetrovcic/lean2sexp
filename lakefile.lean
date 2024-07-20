568a406b2ac60cec789cce18a0385dcc9367cad6
Lean 4 VS Code Extension <>
2024-07-20 16:32:26 +0200

    Initial commit

---
+++ lakefile.lean
@@ -0,0 +1,16 @@
+import Lake
+open Lake DSL
+
+package «lean2sexp» where
+  -- add package configuration options here
+
+lean_lib «Lean2sexp» where
+  -- add library configuration options here
+
+@[default_target]
+lean_exe «lean2sexp» where
+  root := `Main
+  -- Enables the use of the Lean interpreter by the executable (e.g.,
+  -- `runFrontend`) at the expense of increased binary size on Linux.
+  -- Remove this line if you do not need such functionality.
+  supportInterpreter := true
