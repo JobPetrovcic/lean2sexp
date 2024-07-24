f6f176f792e64ede017a0988b0227699a743365c
JobPetrovcic <job1.petrovcic@gmail.com>
2024-07-24 17:12:02 +0200

    changing structure

--- Convert2Sexp.lean
+++ Convert2Sexp.lean
@@ -266,87 +266,55 @@ open Lean
 open Lean.Elab
 open Lean.Elab.Command
 
-structure Config : Type where
-  srcDir : System.FilePath := ".lake/build/lib" -- the directory where .olean files are found
-  outDir : System.FilePath := "sexp" -- the output directory (created if needed)
-  refsOnly : Bool := False -- only output references names
-  force : Bool := False -- process file even if .sexp is newer than .olean
-
-def usage : String :=
-"Usage: lake exe lean2sexp <options>
-
-  --srcdir ⟨DIR⟩    set the source directory where .olean files are (default: build/lib)
-  --outdir ⟨DIR⟩    set the output directory (default: sexp)
-  --refsOnly        output only referenced names instead of syntax trees, much faster (default: false)
-  --force           process a file even if .sexp is newer than .olean (default: false)
-"
--- Poor man's parsing of command-line arguments
-def parseArgs (conf : Config) (args : List String) : Option Config :=
-  match args with
-  | [] => .some conf
-  | "--srcdir" :: dir :: args => parseArgs {conf with srcDir := dir} args
-  | "--outdir" :: dir :: args => parseArgs {conf with outDir := dir} args
-  | "--refsOnly" :: args => parseArgs {conf with refsOnly := True} args
-  | "--force" :: args => parseArgs {conf with force := True} args
-  | _ => .none
-
 unsafe def getConstantsToFullName (nameToModuleId : Lean.Name → Option ModuleIdx) (importedModuleNames : Array Name) (constantName : Name) : Name :=
   match nameToModuleId constantName with
   | some a =>
     importedModuleNames[a.toNat]!.append constantName
   | none => panic! s!"Module ID not found for constant: {constantName}"
 
-unsafe def processModule (moduleName : Name) : IO Unit := do
-  IO.println s!"Processing module {moduleName}"
+unsafe def processModule (moduleName : Name) : IO Sexp := do
+  --IO.println s!"Processing module {moduleName}"
   let env ← importModules #[Import.mk moduleName false] {}
   let nameToModuleId := env.getModuleIdxFor?
   let importedModuleNames := env.header.moduleNames
   let constantsToFullName := getConstantsToFullName nameToModuleId importedModuleNames
-  IO.println s!"Modules {env.header.moduleNames}"
+  --IO.println s!"Modules {env.header.moduleNames}"
   --IO.println s!"Const2ModIdx {env.const2ModIdx.toList.map (fun (k, v) => toString k ++ ": " ++toString v)}"
   let modulesData := env.header.moduleData
   let mainModuleData := modulesData[modulesData.size - 1]!
-  IO.println s!"{Sexp.fromModuleData constantsToFullName false moduleName mainModuleData}"
+  pure (Sexp.fromModuleData constantsToFullName false moduleName mainModuleData)
+
+def nameToSexpFileName (n : Name) : String :=
+  let rec aux : Name → List String
+    | Name.anonymous => []
+    | Name.str p s => aux p ++ [s]
+    | Name.num p n => aux p ++ [toString n]
+  let nameParts := aux n
+  let joinedName := String.intercalate "_" nameParts
+  joinedName ++ ".sexp"
+
+unsafe def recursivelyProcessDirectory (curName : Name)(dir : System.FilePath): IO Unit := do
+  IO.println s!"Searching files in source directory {dir}..."
+  let mut entries ← dir.readDir
+  for entry in entries do
+    if (← entry.path.isDir) then
+      -- is dir
+      let newCurName := Name.str curName entry.fileName
+      IO.println s!"Going into directory {newCurName}"
+      recursivelyProcessDirectory newCurName entry.path
+    else
+      -- is regular file
+      --IO.println s!"Processing {entry.path}"
+      match entry.path.fileStem with
+      | some baseName =>
+        let newCurName := Name.str curName baseName
+        IO.println s!"Processing {newCurName}"
+        let outFile := System.FilePath.mk ("sexp/" ++(nameToSexpFileName newCurName))
+        let sexpConstruct ← (processModule newCurName)
+        IO.FS.withFile outFile .write (fun fh => Sexp.write fh sexpConstruct)
+      | none => IO.println "This should be impossible."
 
 unsafe def main (args : List String) : IO Unit := do
-  match parseArgs ({} : Config) args with
-  | .none =>
-    IO.println s!"Error: could not parse command-line arguments\n\n{usage}"
-  | .some conf =>
-    let what := if conf.refsOnly then "references" else "syntax trees"
-    IO.println s!"Extracting {what} from {conf.srcDir} to {conf.outDir}"
-    IO.FS.createDirAll conf.outDir
-    let allFiles ← System.FilePath.walkDir conf.srcDir
-    let files := allFiles.toList.filter (fun fp => fp.toString.endsWith ".olean")
-    let totalFiles := files.length
-    for (k, srcFile) in files.enumFrom 1 do
-      if ! srcFile.toString.startsWith conf.srcDir.toString then
-        IO.println s!"skipping {srcFile} because, mysteriously, is not in {conf.outDir}"
-      else
-        let baseName := (srcFile.withExtension "").components.drop (conf.srcDir.components.length)
-        let outFile := System.FilePath.join conf.outDir $ (".".intercalate (baseName ++ ["sexp"]))
-        let srcMetadata ← srcFile.metadata
-        let outMetadata ← outFile.metadata.toBaseIO
-        let srcNewer : Bool :=
-          match outMetadata with
-          | .error _ => true
-          | .ok m => decide (srcMetadata.modified > m.modified)
-        if ! conf.force && ! srcNewer then
-          IO.println s!"[{k}/{totalFiles}] SKIPPING {srcFile}"
-        else
-          IO.println s!"[{k}/{totalFiles}] PROCESSING {srcFile} → {outFile}"
-          --let (data, region) ← Lean.readModuleData srcFile
-          let moduleName := baseName.foldl Lean.Name.str Lean.Name.anonymous
-
-          -- Added
-          let env ← importModules #[Import.mk moduleName false] {}
-          let nameToModuleId := env.getModuleIdxFor?
-          let importedModuleNames := env.header.moduleNames
-          let constantsToFullName := getConstantsToFullName nameToModuleId importedModuleNames
-          --IO.println s!"Const2ModIdx {env.const2ModIdx.toList.map (fun (k, v) => toString k ++ ": " ++toString v)}"
-          let modulesData := env.header.moduleData
-          let mainModuleData := modulesData[modulesData.size - 1]!
-          --IO.println s!"{Sexp.fromModuleData constantsToFullName false moduleName mainModuleData}"
-
-          IO.FS.withFile outFile .write (fun fh => Sexp.write fh $ Sexp.fromModuleData constantsToFullName conf.refsOnly moduleName mainModuleData)
-          --region.free
+  match args with
+  | dir :: [] => recursivelyProcessDirectory (Name.str Name.anonymous dir) dir
+  | _ => panic! "Incorrect use of arguments. Input is only the directory."
